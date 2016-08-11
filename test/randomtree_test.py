00775976eae00a42cf4c0c3d349d27436a8af0ea
Nikolaus Leopold <nikolaus.leopold@gmail.com>
2016-08-11 13:11:46 +0200

    added test

---
+++ test/randomtree_test.py
@@ -0,0 +1,8 @@
+import lpy
+l = lpy.Lsystem('/home/niko/Documents/Studium/Semester6/Bachelor Thesis CG/addons/lpy adapted/models/training/10-random-tree.lpy')
+print("FILENAME: {}\n".format(l.filename))
+print("NUMBER OF RULES: {}\n".format(l.nbTotalRules()))
+print("CODE:\n{}\n".format(l.code()))
+print("STRING:\n{}\n".format(l.__str__()))
+print("AXIOM: {}".format(l.axiom))
+print("DERIVATION RESULT:\n{}".format(l.derive()))
